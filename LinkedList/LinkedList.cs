803360daeb124de40f3fc70f1fb81d6260639804
Ritvik5 <sharma.ritvik56@gmail.com>
2023-03-08 20:07:36 +0530

    Inserting a node at Particular Position

--- LinkedList/LinkedList.cs
+++ LinkedList/LinkedList.cs
@@ -40,5 +40,36 @@ namespace LinkedList
                 temp = temp.next;
             }
         }
+
+        internal Node InsertAtParticularPosition(int position, int data)
+        {
+            if (position < 1)
+                Console.WriteLine("Invalid position");
+            if (position == 1)
+            {
+                var newNode = new Node(data);
+                newNode.next = this.head;
+                head = newNode;
+            }
+            else
+            {
+                Node temp = this.head;
+                while (position-- != 0 && temp != null)
+                {
+                    if (position == 1)
+                    {
+                        Node node = new Node(data);
+                        node.next = temp.next;
+                        temp.next = node;
+                        break;
+                    }
+
+                    temp = temp.next;
+                }
+                if (position != 1)
+                    Console.WriteLine("Position out of range");
+            }
+            return head;
+        }
     }
 }
--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -6,8 +6,8 @@
         {
             LinkedList list = new LinkedList();
             list.Add(56);
-            list.Add(30);
             list.Add(70);
+            list.InsertAtParticularPosition(2, 30);
             list.Display();
         }
     }
